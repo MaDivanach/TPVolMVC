0e01a4fe48300b82eb9589c69c6df68ccd9a7f79
Unknown <mathilde.divanach@gmail.com>
2018-08-22 12:20:36 +0200

    Reservation controller et vue list

---
+++ src/main/java/controller/ReservationController.java
@@ -0,0 +1,69 @@
+package controller;
+
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import model.Reservation;
+import repositories.ReservationRepository;
+
+@Controller
+@RequestMapping("/reservation")
+public class ReservationController {
+
+	@Autowired
+	private ReservationRepository reservationRepository;
+	
+	@RequestMapping("")
+	public ModelAndView home() {
+		return new ModelAndView("redirect:/reservation/");
+	}
+
+	@RequestMapping("/")
+	public ModelAndView list() {
+		ModelAndView mv = new ModelAndView("reservation/list");
+		mv.getModelMap().addAttribute("reservations", reservationRepository.findAll());
+		return mv;
+	}
+
+	@RequestMapping("/delete")
+	public String delete(@RequestParam(name = "id") Long id) {
+		reservationRepository.deleteById(id);
+		return "redirect:/reservation/";
+	}
+	
+	@RequestMapping("/edit")
+	public ModelAndView update(@RequestParam(name = "id") Long id) {
+		Optional<Reservation> opt = reservationRepository.findById(id);
+		if(opt.isPresent()) {
+			return goEdit(opt.get());
+		} else {
+			return new ModelAndView("redirect:/reservation/");
+		}
+	}
+	
+	@RequestMapping("/save")
+	public ModelAndView save(@Valid @ModelAttribute("reservation") Reservation reservation, BindingResult br) {
+		if(br.hasErrors()) {
+			return goEdit(reservation);
+		} else {
+			reservationRepository.save(reservation);
+			return new ModelAndView("redirect:/reservation/");
+		}
+	}
+	
+	private ModelAndView goEdit(Reservation reservation) {
+		ModelAndView mv = new ModelAndView("article/edit");
+		mv.getModelMap().addAttribute("reservation", reservation);
+		mv.getModelMap().addAttribute("adherents",reservationRepository.findAll());
+		return mv;
+	}
+}
