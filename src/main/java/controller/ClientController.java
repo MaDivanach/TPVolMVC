682bdbec934d1298f9aacf7e9c8cde617ca9d186
Alfred hitch cock <aurelien.binet34@gmail.com>
2018-08-22 12:01:02 +0200

    page list client : ok

--- src/main/java/controller/ClientController.java
+++ src/main/java/controller/ClientController.java
@@ -6,6 +6,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import model.Client;
+import model.ClientEl;
+import model.ClientMoral;
+import model.ClientPhysique;
 import repositories.ClientRepository;
 
 @Controller
@@ -24,6 +28,27 @@ public class ClientController {
 	@RequestMapping("/")
 	public ModelAndView list(Model model) {
 		return new ModelAndView("client/list", "clients", clientRepository.findAll());
+	}
+	
+	private ModelAndView goEdit(Client client) {
+		ModelAndView modelAndView = new ModelAndView("client/edit", "client", client);
+		modelAndView.addObject("clients", clientRepository.findAll());
+		return modelAndView;
+	}
+	
+	@RequestMapping("/addphysique")
+	public ModelAndView addPhysique() {
+		return goEdit(new ClientPhysique());
+	}
+	
+	@RequestMapping("/addmoral")
+	public ModelAndView addMoral() {
+		return goEdit(new ClientMoral());
+	}
+	
+	@RequestMapping("/addel")
+	public ModelAndView addEl() {
+		return goEdit(new ClientEl());
 	}
 	
 	
